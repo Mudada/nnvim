80c8644cfe14e2883b7d37a2ac128726034fd74e
Tangui <mael.nicolas@clever-cloud.com>
2024-11-10 14:59:50 +0100

    conf theme wezterm

--- wezterm/config.lua
+++ wezterm/config.lua
@@ -1,8 +1,35 @@
-local conf = {
-  font_size = 16,
-  window_background_opacity = 1,
-  hide_tab_bar_if_only_one_tab = true,
-  color_scheme = 'Catppuccin Mocha',
-  window_decorations = "RESIZE",
-  front_end = "WebGpu",
+-- Gonna be read by home-manager, DO NOT RETURN ANYTHING
+
+local wezterm = require 'wezterm'
+
+local conf = wezterm.config_builder()
+
+function get_appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return 'Dark'
+end
+
+function scheme_for_appearance(appearance)
+  if appearance:find 'Dark' then
+    return 'Catppuccin Mocha'
+  else
+    return 'Colorcli (Gogh)'
+  end
+end
+
+conf.font_size = 16
+conf.window_background_opacity = 1
+conf.hide_tab_bar_if_only_one_tab = true
+local theme = wezterm.plugin.require('https://github.com/neapsix/wezterm').dawn
+conf.color_schemes = {
+  ["Catppuccin Latte"] = {
+    background = "#fbfee9",
+    foreground = "black"
+  }
 }
+conf.color_scheme = scheme_for_appearance(get_appearance())
+conf.integrated_title_buttons = { 'Hide', 'Maximize', 'Close' }
+conf.front_end = "WebGpu"
+conf.font = wezterm.font('MonacoB2', { weight = 'Bold' })
