5d8bc26519d78644d826ce4ee9695883bd921baf
Tangui <mael.nicolas@clever-cloud.com>
2025-05-12 15:36:33 +0200

    remove trash

--- modules/default.nix
+++ modules/default.nix
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-{ ... }:
-{
-  imports = [
-    ./nvim
-=======
 {lib, config, username, ... }:
 {
   imports = [
     ./system
     ./home { inherit username; }
->>>>>>> 17e4f43 (WAAAAAAAAAAAAAAAAAAAAAAAA G ISNTALLE UNE FOOOOOONNNNT WAAAAAAAAAAA)
   ];
 }
